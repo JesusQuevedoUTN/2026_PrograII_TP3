2606402992ca38c5ede0b82478f64deb9fee1113
Cheese <jesusquevedomdr@gmail.com>
2026-08-05 22:12:45 -0300

    Ejercicio 2 completo

--- src/Ejercicio2/main.java
+++ src/Ejercicio2/main.java
@@ -5,13 +5,48 @@ Ejercicio 2
  */
 package Ejercicio2;
 
+import Ejercicio1.Alumno;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Iterador: Es una interfaz que permite moverse por distintos elementos de las estructuras de datos
+        //Esta es genérica lo que permite el uso seguro de listas como pilas y colas a las que usualmente
+        //se reestringen ciertos datos, es mucho más pesada en consumo de RAM que el resto de colecciones
+        //y puede ser vista en las funciones for-each
+        
+        ArrayList<Alumno> listaRayada = new ArrayList();
+        Alumno al1 = new Alumno("Jesus");
+        Alumno al2 = new Alumno("Emanuel");
+        Alumno al3 = new Alumno("Quevedo");
+        Alumno al4 = new Alumno("Chacon");
+        listaRayada.add(al1);
+        listaRayada.add(al2);
+        listaRayada.add(al3);
+        listaRayada.add(al4);        
+        
+        Iterator<Alumno> it = listaRayada.iterator();
+
+        System.out.println("\nMostrando con for-each");
+        for (Alumno a : listaRayada) {
+            System.out.println(a.toString());
+        }
+        
+        System.out.println("\nMostrando con while");
+        while(it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.toString());
+        }
+        
+        System.out.println("\nRemoviendo el final");
+        it.remove();
+        
+        for (Alumno a : listaRayada) {
+            System.out.println(a.toString());
+        }
+
     }
 
 }
